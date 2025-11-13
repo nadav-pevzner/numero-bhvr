2cf58a2e15eaded883f67dc5c63a78292afcea20
Nadav Pevzner <nadpev@gmail.com>
2025-11-13 16:08:42 +0200

    add schema to db creation

--- packages/chat-db/src/db.ts
+++ packages/chat-db/src/db.ts
@@ -1,9 +1,10 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as schema from "./schema";
 
 export function createChatDb(connectionString: string) {
 	const pool = new Pool({ connectionString });
-	const db = drizzle({ client: pool });
+	const db = drizzle({ client: pool, schema });
 
 	return db;
 }
